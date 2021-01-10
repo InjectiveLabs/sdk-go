5a6d9f6317fa6a2b25d248053d95403c90464eaf
albertchon <albert@injectiveprotocol.com>
2021-01-09 19:18:43 -0800

    update ZeroExTransaction for futures

--- transaction.go
+++ transaction.go
@@ -70,15 +70,39 @@ func (tx *Transaction) ComputeTransactionHash() (common.Hash, error) {
 	return hash, nil
 }
 
+type Permyriad struct {
+	Value *big.Int
+}
+
 type ZeroExTransactionData struct {
-	FunctionName          ExchangeFunctionName
-	Orders                []*Order
+	FunctionName              FuturesFunctionName
+	Orders                    []*Order
+	MarketIDs                 []common.Hash
+	SubAccountID              common.Hash
+	SubaccountID              common.Hash
+	Signatures                [][]byte
+	PositionID                *big.Int
+	Quantity                  *big.Int
+	BaseCurrency              common.Address
+	Amount                    *big.Int
+	IsRevertingOnPartialFills bool
+	// market specific parameters
+	Ticker                 string
+	Oracle                 common.Address
+	InitialMarginRatio     Permyriad
+	MaintenanceMarginRatio Permyriad
+	FundingInterval        *big.Int
+	ExpirationTime         *big.Int
+	MakerTxFee             Permyriad
+	TakerTxFee             Permyriad
+	RelayerFeePercentage   Permyriad
+	MarketID               common.Hash
+	// parameters from 0x
 	LeftOrders            []*Order
 	RightOrders           []*Order
 	TakerAssetFillAmounts []*big.Int
 	MakerAssetFillAmount  *big.Int
 	TakerAssetFillAmount  *big.Int
-	Signatures            [][]byte
 	LeftSignatures        [][]byte
 	RightSignatures       [][]byte
 }
