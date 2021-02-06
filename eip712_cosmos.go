e92bd501b550c906e54cef8c672b590188f731e4
Maxim <max@injectiveprotocol.com>
2021-02-06 13:52:21 +0300

    Reimplemented WrapTxToEIP712

--- eip712_cosmos.go
+++ eip712_cosmos.go
@@ -3,23 +3,24 @@ package sdk
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"reflect"
 	"strings"
 
+	cosmtypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/pkg/errors"
-	"github.com/xlab/structwalk"
 
 	"github.com/InjectiveLabs/sdk-go/typeddata"
 )
 
 // WrapTxToEIP712 is an ultimate method that wraps Amino-encoded Cosmos Tx JSON data
 // into an EIP712-compatible request. All messages must be of the same type.
-func WrapTxToEIP712(chainID uint64, data []byte) (typeddata.TypedData, error) {
-	message := make(map[string]interface{})
-	if err := json.Unmarshal(data, &message); err != nil {
+func WrapTxToEIP712(chainID uint64, msg cosmtypes.Msg, data []byte) (typeddata.TypedData, error) {
+	txData := make(map[string]interface{})
+	if err := json.Unmarshal(data, &txData); err != nil {
 		err = errors.Wrap(err, "failed to unmarshal data provided into WrapTxToEIP712")
 		return typeddata.TypedData{}, err
 	}
@@ -33,16 +34,16 @@ func WrapTxToEIP712(chainID uint64, data []byte) (typeddata.TypedData, error) {
 	}
 
 	var typedData = typeddata.TypedData{
-		Types:       extractTypes("Tx", message),
+		Types:       extractMsgTypes("MsgValue", msg),
 		PrimaryType: "Tx",
 		Domain:      domain,
-		Message:     message,
+		Message:     txData,
 	}
 
 	return typedData, nil
 }
 
-func extractTypes(rootType string, v map[string]interface{}) typeddata.Types {
+func extractMsgTypes(msgTypeName string, msg cosmtypes.Msg) typeddata.Types {
 	rootTypes := typeddata.Types{
 		"EIP712Domain": {
 			{
@@ -84,60 +85,139 @@ func extractTypes(rootType string, v map[string]interface{}) typeddata.Types {
 		},
 		"Msg": {
 			{Name: "type", Type: "string"},
-			{Name: "value", Type: "MsgValue"},
+			{Name: "value", Type: msgTypeName},
 		},
-		"MsgValue": {},
+		msgTypeName: {},
 	}
 
-	// get first Msg only, all Msgs must be of the same type though
-	msgValue := v["msgs"].([]interface{})[0].(map[string]interface{})["value"]
-
-	const typeDefPrefix = "_."
-	typeDefs := map[string]string{}
-
-	fields := structwalk.FieldListNoSort(msgValue)
-	for _, field := range fields {
-		parent, childName, childType, ok := childTypeInParent(field, msgValue)
-		if !ok {
-			continue
-		}
-
-		if len(parent) == 0 {
-			rootTypes["MsgValue"] = append(rootTypes["MsgValue"], typeddata.Type{
-				Name: childName,
-				Type: childType,
-			})
-
-			continue
-		}
-
-		typeDef := sanitizeTypedef(typeDefPrefix + parent)
-		rootTypes[typeDef] = append(rootTypes[typeDef], typeddata.Type{
-			Name: childName,
-			Type: childType,
-		})
-
-		typeDefs[parent] = parentName(parent)
-	}
-
-	for parent, subParent := range typeDefs {
-		if len(subParent) == 0 {
-			rootTypes["MsgValue"] = append(rootTypes["MsgValue"], typeddata.Type{
-				Name: parent,
-				Type: sanitizeTypedef(typeDefPrefix + parent),
-			})
-
-			continue
-		}
-
-		subParent = sanitizeTypedef(typeDefPrefix + subParent)
-		rootTypes[subParent] = append(rootTypes[subParent], typeddata.Type{
-			Name: parent,
-			Type: sanitizeTypedef(typeDefPrefix + parent),
-		})
-	}
+	walkFields(rootTypes, msgTypeName, msg)
 
 	return rootTypes
+}
+
+const typeDefPrefix = "_"
+
+func walkFields(typeMap typeddata.Types, rootType string, in interface{}) {
+	defer func() {
+		if x := recover(); x != nil {
+			return
+		}
+	}()
+
+	t := reflect.TypeOf(in)
+	v := reflect.ValueOf(in)
+
+	for {
+		if t.Kind() == reflect.Ptr ||
+			t.Kind() == reflect.Interface {
+			t = t.Elem()
+			v = v.Elem()
+
+			continue
+		}
+
+		break
+	}
+
+	traverseFields(typeMap, rootType, typeDefPrefix, t, v)
+}
+
+func traverseFields(typeMap typeddata.Types, rootType string, prefix string, t reflect.Type, v reflect.Value) {
+	n := t.NumField()
+
+	if prefix == typeDefPrefix {
+		if len(typeMap[rootType]) == n {
+			return
+		}
+	} else {
+		typeDef := sanitizeTypedef(prefix)
+		if len(typeMap[typeDef]) == n {
+			return
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		var field reflect.Value
+		if v.IsValid() {
+			field = v.Field(i)
+		}
+
+		fieldType := t.Field(i).Type
+
+		for {
+			if fieldType.Kind() == reflect.Ptr || fieldType.Kind() == reflect.Interface {
+				fieldType = fieldType.Elem()
+
+				if field.IsValid() {
+					field = field.Elem()
+				}
+				continue
+			}
+			break
+		}
+
+		if fieldType.Kind() == reflect.Array {
+			fieldType = fieldType.Elem()
+			field = field.Index(0)
+		}
+
+		for {
+			if fieldType.Kind() == reflect.Ptr || fieldType.Kind() == reflect.Interface {
+				fieldType = fieldType.Elem()
+
+				if field.IsValid() {
+					field = field.Elem()
+				}
+				continue
+			}
+			break
+		}
+
+		fieldName := jsonNameFromTag(t.Field(i).Tag)
+		fieldPrefix := fmt.Sprintf("%s.%s", prefix, fieldName)
+
+		ethTyp := typToEth(fieldType)
+		if len(ethTyp) > 0 {
+			if prefix == typeDefPrefix {
+				typeMap[rootType] = append(typeMap[rootType], typeddata.Type{
+					Name: fieldName,
+					Type: ethTyp,
+				})
+			} else {
+				typeDef := sanitizeTypedef(prefix)
+				typeMap[typeDef] = append(typeMap[typeDef], typeddata.Type{
+					Name: fieldName,
+					Type: ethTyp,
+				})
+			}
+
+			continue
+		}
+
+		if fieldType.Kind() == reflect.Struct {
+			if prefix == typeDefPrefix {
+				typeMap[rootType] = append(typeMap[rootType], typeddata.Type{
+					Name: fieldName,
+					Type: sanitizeTypedef(fieldPrefix),
+				})
+			} else {
+				typeDef := sanitizeTypedef(prefix)
+				typeMap[typeDef] = append(typeMap[typeDef], typeddata.Type{
+					Name: fieldName,
+					Type: sanitizeTypedef(fieldPrefix),
+				})
+			}
+
+			traverseFields(typeMap, rootType, fieldPrefix, fieldType, field)
+			continue
+		}
+	}
+}
+
+func jsonNameFromTag(tag reflect.StructTag) string {
+	jsonTags := tag.Get("json")
+	parts := strings.Split(jsonTags, ",")
+	return parts[0]
 }
 
 // _.foo_bar.baz -> TypeFooBarBaz
@@ -163,43 +243,11 @@ func sanitizeTypedef(str string) string {
 	return buf.String()
 }
 
-// foo.bar.baz = "5" -> FooBar: { Baz: "string" }
-func childTypeInParent(fullPath string, obj interface{}) (parentName, childName, childType string, ok bool) {
-	parts := strings.Split(fullPath, ".")
-	v, _ := structwalk.FieldValue(fullPath, obj)
-
-	childType = typToEth(reflect.TypeOf(v))
-	if len(childType) == 0 {
-		// not a basic type
-		return
-	}
-
-	if len(parts) == 1 {
-		// only root field
-		childName = parts[0]
-		ok = true
-		return
-	}
-
-	parentName = strings.Join(parts[:len(parts)-1], ".")
-	childName = parts[len(parts)-1]
-	ok = true
-	return
-}
-
-func parentName(fullPath string) string {
-	parts := strings.Split(fullPath, ".")
-	if len(parts) < 2 {
-		return ""
-	}
-
-	return strings.Join(parts[:len(parts)-1], ".")
-}
-
 var (
 	hashType    = reflect.TypeOf(common.Hash{})
 	addressType = reflect.TypeOf(common.Address{})
-	bigType     = reflect.TypeOf(big.Int{})
+	bigIntType  = reflect.TypeOf(big.Int{})
+	cosmIntType = reflect.TypeOf(cosmtypes.Int{})
 )
 
 // typToEth supports only basic types and arrays of basic types.
@@ -236,13 +284,15 @@ func typToEth(typ reflect.Type) string {
 			return ethName + "[]"
 		}
 	case reflect.Ptr:
-		if typ.Elem().ConvertibleTo(bigType) {
+		if typ.Elem().ConvertibleTo(bigIntType) ||
+			typ.Elem().ConvertibleTo(cosmIntType) {
 			return "string"
 		}
 	case reflect.Struct:
-		if typ.ConvertibleTo(hashType) {
-			return "string"
-		} else if typ.ConvertibleTo(addressType) {
+		if typ.ConvertibleTo(hashType) ||
+			typ.ConvertibleTo(addressType) ||
+			typ.ConvertibleTo(bigIntType) ||
+			typ.ConvertibleTo(cosmIntType) {
 			return "string"
 		}
 	}
