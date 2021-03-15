ba41aedbf88359c9eff7077ed6d47ad9e6256f4e
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2021-03-15 11:47:09 +0100

    Update peggy types and bump sdk version

--- chain/peggy/types/abi_json.go
+++ chain/peggy/types/abi_json.go
@@ -26,7 +26,8 @@ const (
 			{ "internalType": "address[]", "name": "_destinations",  "type": "address[]" },
 			{ "internalType": "uint256[]", "name": "_fees",          "type": "uint256[]" },
 			{ "internalType": "uint256",   "name": "_batchNonce",    "type": "uint256" },
-			{ "internalType": "address",   "name": "_tokenContract", "type": "address" }
+			{ "internalType": "address",   "name": "_tokenContract", "type": "address" },
+			{ "internalType": "uint256",   "name": "_batchTimeout",  "type": "uint256" }
 		],
 		"outputs": [
 			{ "internalType": "bytes32", "name": "", "type": "bytes32" }
@@ -39,9 +40,9 @@ const (
 		"stateMutability": "pure",
 		"type": "function",
 		"inputs": [
-			{ "internalType": "bytes32",   "name": "_peggyId",     "type": "bytes32" },
-			{ "internalType": "bytes32",   "name": "_checkpoint",  "type": "bytes32" },
-			{ "internalType": "uint256",   "name": "_valsetNonce", "type": "uint256" },
+			{ "internalType": "bytes32",   "name": "_peggyId",     "type": "bytes32"   },
+			{ "internalType": "bytes32",   "name": "_checkpoint",  "type": "bytes32"   },
+			{ "internalType": "uint256",   "name": "_valsetNonce", "type": "uint256"   },
 			{ "internalType": "address[]", "name": "_validators",  "type": "address[]" },
 			{ "internalType": "uint256[]", "name": "_powers",      "type": "uint256[]" }
 		],
@@ -49,4 +50,25 @@ const (
 			{ "internalType": "bytes32", "name": "", "type": "bytes32" }
 		]
 	}]`
+
+	// OutgoingLogicCallABIJSON checks the ETH ABI for compatability of the logic call message
+	OutgoingLogicCallABIJSON = `[{
+	  "name": "checkpoint",
+      "outputs": [],
+      "stateMutability": "pure",
+      "type": "function",
+      "inputs": [
+			{ "internalType": "bytes32",   "name": "_peggyId",                "type": "bytes32"   },
+			{ "internalType": "bytes32",   "name": "_methodName",             "type": "bytes32"   },
+			{ "internalType": "uint256[]", "name": "_transferAmounts",        "type": "uint256[]" },
+			{ "internalType": "address[]", "name": "_transferTokenContracts", "type": "address[]" },
+			{ "internalType": "uint256[]", "name": "_feeAmounts",             "type": "uint256[]" },
+			{ "internalType": "address[]", "name": "_feeTokenContracts",      "type": "address[]" },
+			{ "internalType": "address",   "name": "_logicContractAddress",   "type": "address"   },
+			{ "internalType": "bytes",     "name": "_payload",                "type": "bytes"     },
+			{ "internalType": "uint256",   "name": "_timeout",                "type": "uint256"   },
+			{ "internalType": "bytes32",   "name": "_invalidationId",         "type": "bytes32"   },
+			{ "internalType": "uint256",   "name": "_invalidationNonce",      "type": "uint256"   }
+      ]
+    }]`
 )
