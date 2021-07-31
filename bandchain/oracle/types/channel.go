f6981378416ed88ac42755fb56889adbe322f395
mankenavenkatesh <mankenavenkatesh@gmail.com>
2021-07-31 07:04:57 +0530

    Remove unnecesssary file

--- bandchain/oracle/types/channel.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// NewIBCChannel creates a new IBCChannel instance.
-func NewIBCChannel(portId, channelId string) IBCChannel {
-	return IBCChannel{
-		PortId:    portId,
-		ChannelId: channelId,
-	}
-}
