88b0d22126a97e8fd4864395ec0d43d38ec410c0
nhannamsiu <dangduynam95@gmail.com>
2022-03-12 21:10:04 +0700

    Update chain example using batch broadcast

---
+++ examples/chain/0_MsgSend.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"github.com/InjectiveLabs/sdk-go/client"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+	"time"
+)
+
+func main() {
+	network := client.LoadNetwork("testnet", "k8s")
+	tmRPC, err := rpchttp.New(network.TmEndpoint, "/websocket")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	senderAddress, cosmosKeyring, err := client.InitCosmosKeyring(
+		"/Users/nam/.injectived",
+		"injectived",
+		"file",
+		"inj-user",
+		"12345678",
+		"5d386fbdbf11f1141010f81a46b40f94887367562bd33b452bbaa6ce1cd1381e", // keyring will be used if pk not provided
+		false,
+	)
+
+	clientCtx, err := client.NewClientContext(
+		network.ChainId,
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+	clientCtx.WithNodeURI(network.TmEndpoint)
+	clientCtx = clientCtx.WithClient(tmRPC)
+
+	msg := &banktypes.MsgSend{
+		FromAddress: senderAddress.String(),
+		ToAddress:   "inj1hkhdaj2a2clmq5jq6mspsggqs32vynpk228q3r",
+		Amount: []sdktypes.Coin{
+			{Denom: "inj", Amount: sdktypes.NewInt(1)},
+		},
+	}
+
+	chainClient, err := client.NewChainClient(
+		clientCtx,
+		network.ChainGrpcEndpoint,
+		client.OptionTLSCert(network.ChainTlsCert),
+		client.OptionGasPrices("500000000inj"),
+	)
+
+	for i:=0; i<2; i++ {
+		err := chainClient.QueueBroadcastMsg(msg)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	time.Sleep(time.Second * 2)
+	for i:=0; i<2; i++ {
+		err := chainClient.QueueBroadcastMsg(msg)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	for true {}
+}
