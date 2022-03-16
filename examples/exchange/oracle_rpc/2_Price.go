6b3101dcaeae158f7875f7199e5bfaa9465f3420
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-03-16 22:00:37 +0200

    feat: add examples for OracleRPC

---
+++ examples/exchange/oracle_rpc/2_Price.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/InjectiveLabs/sdk-go/client/common"
+	exchangeclient "github.com/InjectiveLabs/sdk-go/client/exchange"
+)
+
+func main() {
+	network := common.LoadNetwork("mainnet", "lb")
+	exchangeClient, err := exchangeclient.NewExchangeClient(network.ExchangeGrpcEndpoint, common.OptionTLSCert(network.ExchangeTlsCert))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ctx := context.Background()
+	baseSymbol := "BTC"
+	quoteSymbol := "USDT"
+	oracleType := "BandIBC"
+	oracleScaleFactor := uint32(6)
+	res, err := exchangeClient.GetPrice(ctx, baseSymbol, quoteSymbol, oracleType, oracleScaleFactor)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
+}
