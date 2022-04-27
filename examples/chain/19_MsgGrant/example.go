6d63e6d36b3b8f771617a30e6e0d3704e7f99536
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-04-27 11:48:51 +0300

    chore: update MsgGrant example

--- examples/chain/19_MsgGrant/example.go
+++ examples/chain/19_MsgGrant/example.go
@@ -56,20 +56,11 @@ func main() {
 	grantee := "inj1hkhdaj2a2clmq5jq6mspsggqs32vynpk228q3r"
 	expireIn := time.Now().AddDate(1, 0, 0) // years months days
 
-	// GENERIC AUTHZ
+	//GENERIC AUTHZ
 	//msgtype := "/injective.exchange.v1beta1.MsgCreateSpotLimitOrder"
 	//msg := chainClient.BuildGenericAuthz(granter, grantee, msgtype, expireIn)
 
 	// TYPED AUTHZ
-	//msg := chainClient.BuildExchangeBatchUpdateOrdersAuthz(
-	//	granter,
-	//	grantee,
-	//	chainClient.DefaultSubaccount(senderAddress).String(),
-	//	[]string{"0xe0dc13205fb8b23111d8555a6402681965223135d368eeeb964681f9ff12eb2a"},
-	//	[]string{"0xe0dc13205fb8b23111d8555a6402681965223135d368eeeb964681f9ff12eb2a"},
-	//	expireIn,
-	//)
-
 	msg := chainClient.BuildExchangeAuthz(
 		granter,
 		grantee,
