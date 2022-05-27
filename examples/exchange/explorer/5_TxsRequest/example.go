c84666c8f3a7a69b38e2a171612e64d00752788f
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-05-27 17:02:43 +0300

    feat: add more methods in ExplorerRPC

---
+++ examples/exchange/explorer/5_TxsRequest/example.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/InjectiveLabs/sdk-go/client/common"
+	exchangeclient "github.com/InjectiveLabs/sdk-go/client/exchange"
+)
+
+func main() {
+	//network := common.LoadNetwork("mainnet", "k8s")
+	network := common.LoadNetwork("testnet", "k8s")
+	exchangeClient, err := exchangeclient.NewExchangeClient(network.ExchangeGrpcEndpoint, common.OptionTLSCert(network.ExchangeTlsCert))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	ctx := context.Background()
+	res, err := exchangeClient.GetTxs(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	str, _ := json.MarshalIndent(res, "", " ")
+	fmt.Print(string(str))
+}
