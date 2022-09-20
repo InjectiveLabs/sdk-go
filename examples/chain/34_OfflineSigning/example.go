c2438efc4ae2d67b74819a0fed7a3445f8a250be
vinhphuctadang <vinhphuctadang@gmail.com>
2022-09-20 22:59:03 +0700

    feat: add get accountNonce without increasing seq

---
+++ examples/chain/34_OfflineSigning/example.go
@@ -0,0 +1,119 @@
+// example for how to sign tx offline, store to file and load + broadcast later
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/InjectiveLabs/sdk-go/client/common"
+	"github.com/shopspring/decimal"
+
+	exchangetypes "github.com/InjectiveLabs/sdk-go/chain/exchange/types"
+	chainclient "github.com/InjectiveLabs/sdk-go/client/chain"
+	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
+)
+
+func StoreTxToFile(fileName string, txBytes []byte) error {
+	return ioutil.WriteFile(fileName, txBytes, 0755)
+}
+
+func LoadTxFromFile(fileName string) ([]byte, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	txBytes, err := ioutil.ReadAll(f)
+	return txBytes, nil
+}
+
+func main() {
+	network := common.LoadNetwork("devnet", "")
+	tmRPC, err := rpchttp.New(network.TmEndpoint, "/websocket")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	senderAddress, cosmosKeyring, err := chainclient.InitCosmosKeyring(
+		os.Getenv("HOME")+"/.injectived",
+		"injectived",
+		"file",
+		"inj-user",
+		"12345678",
+		"5d386fbdbf11f1141010f81a46b40f94887367562bd33b452bbaa6ce1cd1381e", // keyring will be used if pk not provided
+		false,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	clientCtx, err := chainclient.NewClientContext(
+		network.ChainId,
+		senderAddress.String(),
+		cosmosKeyring,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	clientCtx = clientCtx.WithNodeURI(network.TmEndpoint).WithClient(tmRPC)
+
+	chainClient, err := chainclient.NewChainClient(
+		clientCtx,
+		network.ChainGrpcEndpoint,
+		common.OptionTLSCert(network.ChainTlsCert),
+		common.OptionGasPrices("500000000inj"),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defaultSubaccountID := chainClient.DefaultSubaccount(senderAddress)
+
+	marketId := "0xa508cb32923323679f29a032c70342c147c17d0145625922b0ef22e955c844c0"
+
+	amount := decimal.NewFromFloat(2)
+	price := decimal.NewFromFloat(1.02)
+
+	order := chainClient.SpotOrder(defaultSubaccountID, network, &chainclient.SpotOrderData{
+		OrderType:    exchangetypes.OrderType_BUY, //BUY SELL BUY_PO SELL_PO
+		Quantity:     amount,
+		Price:        price,
+		FeeRecipient: senderAddress.String(),
+		MarketId:     marketId,
+	})
+
+	msg := new(exchangetypes.MsgCreateSpotLimitOrder)
+	msg.Sender = senderAddress.String()
+	msg.Order = exchangetypes.SpotOrder(*order)
+
+	accNum, accSeq := chainClient.GetAccNonce()
+	signedTx, err := chainClient.BuildSignedTx(clientCtx, accNum, accSeq, msg)
+	if err != nil {
+		panic(err)
+	}
+
+	// store signed tx into file
+	err = StoreTxToFile("msg.dat", signedTx)
+	if err != nil {
+		panic(err)
+	}
+
+	// load from file and broadcast the signed tx
+	signedTxBytesFromFile, err := LoadTxFromFile("msg.dat")
+	if err != nil {
+		panic(err)
+	}
+
+	txResult, err := chainClient.SyncBroadcastSignedTx(signedTxBytesFromFile)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("txResult:", txResult)
+}
