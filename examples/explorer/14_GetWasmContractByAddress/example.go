5a633d31d3f5186c3f21f7e2f49c336266741a65
vinhphuctadang <vinhphuctadang@gmail.com>
2023-02-28 16:05:32 +0700

    chore: add explorer examples

---
+++ examples/explorer/14_GetWasmContractByAddress/example.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	explorerPB "github.com/InjectiveLabs/sdk-go/exchange/explorer_rpc/pb"
+
+	"github.com/InjectiveLabs/sdk-go/client/common"
+	explorerclient "github.com/InjectiveLabs/sdk-go/client/explorer"
+)
+
+func main() {
+	network := common.LoadNetwork("testnet", "k8s")
+	explorerClient, err := explorerclient.NewExplorerClient(network.ExplorerGrpcEndpoint, common.OptionTLSCert(network.ExplorerTlsCert))
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	req := explorerPB.GetWasmContractByAddressRequest{
+		ContractAddress: "inj1ru9nhdjtjtz8u8wrwxmcl9zsns4fh2838yr5ga",
+	}
+	res, err := explorerClient.GetWasmContractByAddress(ctx, req)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	str, _ := json.MarshalIndent(res, "", " ")
+	fmt.Print(string(str))
+}
