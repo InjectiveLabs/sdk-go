491111d9b40cb6c740e718eda05c44aea473d282
Michele Meloni <cleaversdev@gmail.com>
2023-03-21 12:42:36 +0100

    chore: upgrade sdk with timestamps on portfolio stream api

--- examples/exchange/portfolio/2_StreamAccountPortfolioBankBalances/example.go
+++ examples/exchange/portfolio/2_StreamAccountPortfolioBankBalances/example.go
@@ -5,21 +5,20 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/InjectiveLabs/sdk-go/client/common"
 	exchangeclient "github.com/InjectiveLabs/sdk-go/client/exchange"
 )
 
 func main() {
 	// select network: local, testnet, mainnet
-	network := common.LoadNetwork("testnet", "k8s")
-	exchangeClient, err := exchangeclient.NewExchangeClient(network.ExchangeGrpcEndpoint, common.OptionTLSCert(network.ExchangeTlsCert))
+	//network := common.LoadNetwork("testnet", "k8s")
+	exchangeClient, err := exchangeclient.NewExchangeClient("localhost:9907")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	ctx := context.Background()
 
-	stream, err := exchangeClient.StreamAccountPortfolio(ctx, "inj1clw20s2uxeyxtam6f7m84vgae92s9eh7vygagt", "", "")
+	stream, err := exchangeClient.StreamAccountPortfolio(ctx, "inj1rgxjfea3y2e7n0frz5syly8n5zulagy3fc56jy", "", "")
 	if err != nil {
 		fmt.Println(err)
 	}
