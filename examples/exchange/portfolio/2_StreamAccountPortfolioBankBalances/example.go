9fbecd59cc65e50dd1c6de944de91366eba11c3e
nhannamsiu <dangduynam95@gmail.com>
2023-05-06 14:03:58 +0700

    chore: revert local test example

--- examples/exchange/portfolio/2_StreamAccountPortfolioBankBalances/example.go
+++ examples/exchange/portfolio/2_StreamAccountPortfolioBankBalances/example.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/InjectiveLabs/sdk-go/client/common"
 
 	exchangeclient "github.com/InjectiveLabs/sdk-go/client/exchange"
 )
 
 func main() {
 	// select network: local, testnet, mainnet
-	//network := common.LoadNetwork("testnet", "k8s")
-	exchangeClient, err := exchangeclient.NewExchangeClient("localhost:9907")
+	network := common.LoadNetwork("testnet", "k8s")
+	exchangeClient, err := exchangeclient.NewExchangeClient(network.ExchangeGrpcEndpoint, common.OptionTLSCert(network.ExchangeTlsCert))
 	if err != nil {
 		fmt.Println(err)
 	}
