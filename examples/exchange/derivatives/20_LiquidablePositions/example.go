17963637ef94c8b3b9417f660d26ef36878b3bdd
abel <abarmoa@gmail.com>
2023-10-06 16:45:19 -0300

    (fix) Fix network config in liquidable positions example

--- examples/exchange/derivatives/20_LiquidablePositions/example.go
+++ examples/exchange/derivatives/20_LiquidablePositions/example.go
@@ -11,21 +11,21 @@ import (
 )
 
 func main() {
-	network := common.LoadNetwork("testnet", "sentry1")
+	network := common.LoadNetwork("testnet", "lb")
 	exchangeClient, err := exchangeclient.NewExchangeClient(network)
 	if err != nil {
 		panic(err)
 	}
 
 	ctx := context.Background()
-	marketId := "0x17ef48032cb24375ba7c2e39f384e56433bcab20cbee9a7357e4cba2eb00abe6"
+	//marketId := "0x17ef48032cb24375ba7c2e39f384e56433bcab20cbee9a7357e4cba2eb00abe6"
 	skip := uint64(0)
 	limit := int32(10)
 
 	req := derivativeExchangePB.LiquidablePositionsRequest{
-		MarketId: marketId,
-		Skip:     skip,
-		Limit:    limit,
+		//MarketId: marketId,
+		Skip:  skip,
+		Limit: limit,
 	}
 
 	res, err := exchangeClient.GetDerivativeLiquidablePositions(ctx, req)
